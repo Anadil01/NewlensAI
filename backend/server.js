aeaf6121acdba8217a20cd5a6b1962f0f917c04f
Anadil <mdanadil32@gmail.com>
2026-05-07 13:12:20 +0530

    fix cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const scrapeStories = require("./services/scraperService");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
